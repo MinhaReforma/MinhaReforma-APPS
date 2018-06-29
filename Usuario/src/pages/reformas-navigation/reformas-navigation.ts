12432d3882cd8786abd3c12d7962e2ddf7c19448
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2018-06-29 00:15:29 -0300

    [Usuario] Colocando conteúdo de reformas na view

--- Usuario/src/pages/reformas-navigation/reformas-navigation.ts
+++ Usuario/src/pages/reformas-navigation/reformas-navigation.ts
@@ -1,12 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ReformasNavigationPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IonicPage, ModalController, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -16,13 +10,51 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class ReformasNavigationPage {
 
   tiposReformas:string;
+  id: number;
+  reformas:any = [];
+  API_URL: string = "https://minhareforma.herokuapp.com/";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, public httpClient: HttpClient) {
     this.tiposReformas = 'minhas';
+    this.id = navParams.get("id");
+    console.log(this.id);
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ReformasNavigationPage');
+  criarReforma(){
+    let modal = this.modalCtrl.create('CriaReformaPage', {"id":this.id});
+    modal.onDidDismiss(data => {
+      this.carregaReformas();
+    });
+    modal.present();
+
+
+  }
+
+  ionViewDidLoad(){
+    this.carregaReformas();
+  }
+
+  async carregaReformas(){
+    return await new Promise((resolve, reject) => {
+      let url = this.API_URL + "reformas";
+
+      this.httpClient.get(url).subscribe(
+        (result: any) => {
+          if(result){
+            for(let i in result){
+              this.reformas.push(result[i]);
+            }
+
+
+          }
+          //resolve(result.json());
+        },
+        error => {
+          //reject(error.json());
+        }
+      );
+    });
   }
 
 }
