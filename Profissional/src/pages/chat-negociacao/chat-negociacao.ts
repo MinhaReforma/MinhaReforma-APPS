2463bfc847f042b67d5df5b27850efa3764d4e50
Unknown <nicollas.bastos@ufrpe.br>
2018-07-12 00:34:17 -0300

    Rotas de Chat

--- Profissional/src/pages/chat-negociacao/chat-negociacao.ts
+++ Profissional/src/pages/chat-negociacao/chat-negociacao.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { HttpClient } from '@angular/common/http';
+import { Component, NgZone } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the ChatNegociacaoPage page.
@@ -10,16 +12,70 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-chat-negociacao',
-  templateUrl: 'chat-negociacao.html',
+  selector: "page-chat-negociacao",
+  templateUrl: "chat-negociacao.html"
 })
 export class ChatNegociacaoPage {
+  API_URL: string = "https://minhareforma.herokuapp.com/";
+  idReforma: any;
+  profissional: any;
+  id: any;
+  conversa: any;
+  mensagem: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public httpClient: HttpClient,
+    public storage: Storage,
+    public ngZone: NgZone,
+    public toastCtrl: ToastController
+  ) {
+    this.idReforma = navParams.get("id");
+    this.storage.get("profissional").then(val => {
+      this.profissional = val;
+    });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ChatNegociacaoPage');
+    setInterval(this.carregaChat(), 1000);
   }
 
+  retornaPagina(){
+    this.navCtrl.pop();
+  }
+
+  async carregaChat() {
+    let url =
+      this.API_URL + "conversa/" + this.idReforma + "/" + this.profissional.id;
+    this.httpClient.get(url).subscribe(
+      data => {
+        this.ngZone.run(() => {
+          this.conversa = data;
+        });
+      },
+      err => {}
+    );
+  }
+
+  async enviaMensagem() {
+    let url = this.API_URL + "/conversa/mensagem";
+    this.httpClient
+      .post(url, {
+        id_conversa: this.conversa.id,
+        perfil: "profissional",
+        data: Date.now(),
+        mensagem: this.mensagem
+      })
+      .subscribe(
+        data => {},
+        err => {
+          this.toastCtrl.create({
+            message: err.message,
+            duration: 1500,
+            position: 'bottom'
+          }).present();
+        }
+      );
+  }
 }
--- Usuario/src/pages/chat-negociacao/chat-negociacao.ts
+++ Usuario/src/pages/chat-negociacao/chat-negociacao.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { Component, NgZone } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 /**
  * Generated class for the ChatNegociacaoPage page.
@@ -14,12 +16,69 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'chat-negociacao.html',
 })
 export class ChatNegociacaoPage {
+  API_URL: string = "https://minhareforma.herokuapp.com/";
+  idReforma: any;
+  cliente: any;
+  id: any;
+  conversa: any;
+  mensagem: any;
+  idProfissional: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public httpClient: HttpClient,
+    public storage: Storage,
+    public ngZone: NgZone,
+    public toastCtrl: ToastController
+  ) {
+    this.idProfissional = navParams.get("idProfissional");
+    this.idReforma = navParams.get("id");
+    this.storage.get("cliente").then(val => {
+      this.cliente = val;
+    });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ChatNegociacaoPage');
+    setInterval(this.carregaChat(), 1000);
   }
 
+  retornaPagina(){
+    this.navCtrl.pop();
+  }
+
+  async carregaChat() {
+    let url =
+      this.API_URL + "conversa/" + this.idReforma + "/" + this.idProfissional;
+    this.httpClient.get(url).subscribe(
+      data => {
+        this.ngZone.run(() => {
+          this.conversa = data;
+        });
+      },
+      err => {}
+    );
+  }
+
+  async enviaMensagem() {
+    let url = this.API_URL + "/conversa/mensagem";
+    this.httpClient
+      .post(url, {
+        id_conversa: this.conversa.id,
+        perfil: "cliente",
+        data: Date.now().toString(),
+        mensagem: this.mensagem
+      })
+      .subscribe(
+        data => {},
+        err => {
+          this.toastCtrl.create({
+            message: err.message,
+            duration: 1500,
+            position: 'bottom'
+          }).present();
+        }
+      );
+  }
 }
+
